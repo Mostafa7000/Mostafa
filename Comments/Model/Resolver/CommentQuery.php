69d349f1d0e8da76218358d007aed20685319267
Mostafa <mostafatarek7000@gmail.com>
2023-08-25 03:12:25 +0300

    Add comment to cart

---
+++ Comments/Model/Resolver/CommentQuery.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mostafa\Comments\Model\Resolver;
+
+use Magento\Framework\Exception\AlreadyExistsException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+use Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface;
+use Magento\Quote\Model\ResourceModel\Quote\QuoteIdMask as QuoteIdMaskResourceModel;
+use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
+
+class CommentQuery implements ResolverInterface
+{
+    public function __construct(
+       private GetCartForUser $getCartForUser
+    )
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+        /** @var Quote $cart */
+        $cart = $value['model'];
+
+        return $cart->getCustomerNote();
+    }
+}
---
+++ Comments/Model/Resolver/SetCommentMutation.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Mostafa\Comments\Model\Resolver;
+
+use Exception;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Quote\Model\QuoteRepository\SaveHandler;
+use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
+
+class SetCommentMutation implements ResolverInterface
+{
+
+    public function __construct(
+        private readonly GetCartForUser $getCartForUser,
+        private readonly SaveHandler    $cartSaveHandler,
+    )
+    {
+    }
+
+    /**
+     * Fetches the data from persistence models and format it according to the GraphQL schema.
+     *
+     * @param Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @return array
+     * @throws Exception
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): array
+    {
+        if (empty($args['input']['cart_id'])) {
+            throw new GraphQlInputException(__('Required parameter "cart_id" is missing'));
+        }
+        $maskedCartId = $args['input']['cart_id'];
+
+        if (empty($args['input']['comment'])) {
+            throw new GraphQlInputException(__('Required parameter "comment" is missing'));
+        }
+        $comment = $args['input']['comment'];
+
+        $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        $cart = $this->getCartForUser->execute($maskedCartId, $context->getUserId(), $storeId);
+        $cart->setCustomerNote($comment);
+        $this->cartSaveHandler->save($cart);
+
+        return [
+            'model' => $cart,
+        ];
+    }
+}
